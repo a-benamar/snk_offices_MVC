929558186a214fc926a875cb51374999a851dc27
A. BEN AMAR <abenamar@gmx.com>
2022-03-22 00:29:52 +0100

    better

--- model/bdd.php
+++ model/bdd.php
@@ -8,11 +8,19 @@
 
 function etablirCo()
 {
-    $urlSGBD  = "localhost"; 
-    $nomBDD   = "snk_offices";
-    $loginBDD = "root";
-    $mdpBDD   = "";
-    $dsn      = "mysql:host=$urlSGBD; dbname=$nomBDD"; // data source name
+     // dev connexion
+    // $urlSGBD  = "localhost"; 
+    // $nomBDD   = "snk_offices";
+    // $loginBDD = "root";
+    // $mdpBDD   = "";
+    // $dsn      = "mysql:host=$urlSGBD; dbname=$nomBDD"; // data source name
+
+    // remote database connexion
+    $host  = "remotemysql.com"; 
+    $nomBDD   = "NQwqORDCXH";
+    $loginBDD = "NQwqORDCXH";
+    $mdpBDD   = "nWq5tmfAI8";
+    $dsn      = "mysql:host=$host; dbname=$nomBDD"; // data source name
 
     try {
         $connex = new PDO("$dsn", "$loginBDD", "$mdpBDD", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
